e9748efcbe3c3d708db742d8c7e8091f7bd3787a
Damien Forthomme <dforthom@amazon.com>
2023-10-11 10:53:56 -0400

    first push

--- setup.py
+++ setup.py
@@ -2,13 +2,6 @@ import os
 from pathlib import Path
 from setuptools import find_packages, setup
 
-# Declare your non-python data files:
-# Files underneath configuration/ will be copied into the build preserving the
-# subdirectory structure if they exist.
-package_path = Path(__file__).parent
-data_files = [
-    str(version_path.relative_to(package_path)),
-]
 install_requires = (
     Path(__file__).parent.joinpath("requirements.txt").read_text().splitlines()
 )
@@ -20,6 +13,5 @@ setup(
     packages=find_packages(where="src", exclude=("test",)),
     package_dir={"": "src"},
     include_package_data=True,
-    data_files=data_files,
     python_requires='>3.9.0',
 )
